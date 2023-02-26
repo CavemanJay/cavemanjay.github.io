059dc674c59d87468f8eb6c50d9f1aaf39b98a95
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2023-02-26 00:47:55 -0500

    Update JSF visualizer

--- src/pages/projects/jsf_visualizer.rs
+++ src/pages/projects/jsf_visualizer.rs
@@ -1,10 +1,12 @@
 use jsfuckrs;
 use wasm_bindgen::JsCast;
-use web_sys::{EventTarget, HtmlInputElement, HtmlTextAreaElement};
+use web_sys::{window, Document, EventTarget, HtmlInputElement, HtmlTextAreaElement, Node};
 use yew::prelude::*;
 
 #[derive(PartialEq, Properties)]
 pub struct JsfVisualizerProps {}
+
+// TODO: Give ability for user to both console.log and alert output
 
 #[function_component]
 pub fn JsfVisualizer(props: &JsfVisualizerProps) -> Html {
@@ -12,10 +14,20 @@ pub fn JsfVisualizer(props: &JsfVisualizerProps) -> Html {
     let input_value = (*input_value_handle).clone();
     let input_value_compile = (*input_value_handle).clone();
     let output_value_handle = use_state(String::new);
+    let output_value_exe = (*output_value_handle).clone();
     let output_value = (*output_value_handle).clone();
 
+    let execute = move |_| {
+        let y = js_sys::eval(&output_value_exe).unwrap();
+        gloo_console::log!(y);
+    };
     let compile = move |_| {
-        let compiled = jsfuckrs::lbp::compile(input_value_compile.clone());
+        let src = input_value_compile.clone();
+        if src.trim() == "" {
+            return;
+        }
+
+        let compiled = jsfuckrs::lbp::compile(src);
         output_value_handle.set(compiled);
     };
     let onchange = Callback::from(move |e: Event| {
@@ -26,13 +38,13 @@ pub fn JsfVisualizer(props: &JsfVisualizerProps) -> Html {
     });
     html! {
         <div>
-            // <label>{&input_value}</label>
             <input class="m-10 [writing-mode:horizontal-tb]" {onchange} value={input_value.clone()} />
             <button class="border rounded p-2" onclick={compile}>{"Compile"}</button>
             <hr/>
-            <div class="max-w-lg max-h-80 overflow-auto">
+            <div class="max-w-lg h-80 overflow-auto border border-[var(--secondary)] rounded m-2">
                 <code>{&output_value}</code>
             </div>
+            <button class="border mx-2 rounded p-2 border-[var(--secondary)]" onclick={execute}>{"Run This"}</button>
         </div>
     }
 }
