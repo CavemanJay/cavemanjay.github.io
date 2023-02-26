354dfce00a5093609f15ebd2c2a3d5eecd7fa98a
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2023-02-26 12:30:13 -0500

    Simplify state logic

--- src/pages/projects/jsf_visualizer.rs
+++ src/pages/projects/jsf_visualizer.rs
@@ -10,39 +10,45 @@ pub struct JsfVisualizerProps {}
 
 #[function_component]
 pub fn JsfVisualizer(props: &JsfVisualizerProps) -> Html {
-    let input_value_handle = use_state(|| AttrValue::from("alert(1)"));
-    let input_value = (*input_value_handle).clone();
-    let input_value_compile = (*input_value_handle).clone();
-    let output_value_handle = use_state(String::new);
-    let output_value_exe = (*output_value_handle).clone();
-    let output_value = (*output_value_handle).clone();
+    let js_state = use_state(|| AttrValue::from("alert(1)"));
+    let output_value = use_state(String::new);
 
-    let execute = move |_| {
-        let y = js_sys::eval(&output_value_exe).unwrap();
-        gloo_console::log!(y);
+    let execute = {
+        let output_value = output_value.clone();
+        Callback::from(move |_| {
+            let y = js_sys::eval(&output_value).unwrap();
+            gloo_console::log!(y);
+        })
     };
-    let compile = move |_| {
-        let src = input_value_compile.clone();
-        if src.trim() == "" {
-            return;
-        }
+    let compile = {
+        let src = js_state.clone();
+        let output = output_value.clone();
+        Callback::from(move |_| {
+            if src.trim() == "" {
+                return;
+            }
 
-        let compiled = jsfuckrs::lbp::compile(src);
-        output_value_handle.set(compiled);
+            let compiled = jsfuckrs::lbp::compile(&*src);
+            output.set(compiled);
+        })
     };
-    let onchange = Callback::from(move |e: Event| {
-        let target: EventTarget = e
-            .target()
-            .expect("Event should have a target when dispatched");
-        input_value_handle.set(target.unchecked_into::<HtmlInputElement>().value().into());
-    });
+    let onchange = {
+        let js_state = js_state.clone();
+        Callback::from(move |e: Event| {
+            let target: EventTarget = e
+                .target()
+                .expect("Event should have a target when dispatched");
+            js_state.set(target.unchecked_into::<HtmlInputElement>().value().into());
+        })
+    };
+
     html! {
         <div>
-            <input class="m-10 [writing-mode:horizontal-tb]" {onchange} value={input_value.clone()} />
+            <input class="m-10 [writing-mode:horizontal-tb]" {onchange} value={js_state.as_str().to_string()} />
             <button class="border rounded p-2" onclick={compile}>{"Compile"}</button>
             <hr/>
             <div class="max-w-lg h-80 overflow-auto border border-[var(--secondary)] rounded m-2">
-                <code>{&output_value}</code>
+                <code>{&*output_value}</code>
             </div>
             <button class="border mx-2 rounded p-2 border-[var(--secondary)]" onclick={execute}>{"Run This"}</button>
         </div>
