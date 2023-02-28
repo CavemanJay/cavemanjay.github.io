5928b283b8ba345c970c79ad7732ba2bead6f5f2
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2023-02-28 14:54:38 -0500

    Update padding on smaller screens

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -4,10 +4,9 @@ use leptos_router::*;
 
 #[component]
 pub fn Home(cx: Scope) -> impl IntoView {
-    // let _navigator = use_navigator().unwrap();
     view! { cx,
         <>
-            <div class="home">
+            <div class="max-w-2xl mx-auto sm:py-16 sm:px-8 py-4 px-2">
                 <header>
                     <h1 class="text-[2em] font-[monospace] font-bold">"/optsec"</h1>
                     <p></p>
