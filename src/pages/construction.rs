fc61791d81a29dafc1c5d9735d0d52aaca103ce5
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2023-02-28 15:17:05 -0500

    Adjust image size on smaller screens

--- src/pages/construction.rs
+++ src/pages/construction.rs
@@ -8,7 +8,7 @@ pub fn UnderConstruction(cx: Scope) -> impl IntoView {
         <div class="w-full m-0 absolute top-[50%] translate-y-[-50%]">
             <p class="text-center text-2xl">{"Uh oh..."}</p>
             <p class="text-center secondary">{"I haven't quite finished this page yet. Check back later :)"}</p>
-            <img class="w-[50%]" src="/static/yikes.jpg"/>
+            <img class="sm:w-[50%]" src="/static/yikes.jpg"/>
         </div>
     }
 }
