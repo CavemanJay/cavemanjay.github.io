9d0d3e0a34f48bdbdac5fdb69556311846bc94b5
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2023-02-27 23:23:27 -0500

    Update site styling and jsf project

--- src/components/app.rs
+++ src/components/app.rs
@@ -6,69 +6,71 @@ use leptos_router::*;
 pub fn App(cx: Scope) -> impl IntoView {
 
     view! { cx,
-        <Router>
-            // <nav>
-            //     <A exact=true href=Page::Home.path()>"Home"</A>
-            //     <A exact=true href=Page::About.path()>"About"</A>
-            //     <A href=Page::Projects.path()>"Projects"</A>
-            // </nav>
-            <main>
-                <Routes>
-                    <Route
-                        path=Page::Root.path()
-                        view=move |cx| view! { cx,  <Redirect path=Page::Home.path() /> }
-                    />
-                    <Route
-                        // path="/home"
-                        path=Page::Home.path()
-                        view=move |cx| view! { cx,  <Home/> }
-                    />
-                    <Route
-                        // path="/about"
-                        path=Page::About.path()
-                        view=move |cx| view! { cx,  <About/> }
-                    />
-                    <Route
-                        path=Page::BrainF___.path()
-                        view=move |cx| view! { cx,  <UnderConstruction /> }
-                    />
-                    <Route
-                        path=Page::JSF___.path()
-                        view=move |cx| view! { cx,  <JsfVisualizer /> }
-                    />
-                    <Route
-                        path=Page::TrueJson.path()
-                        view=move |cx| view! { cx,  <UnderConstruction /> }
-                    />
-                    <Route
-                        path="*"
-                        view=move |cx| view! { cx,  <NotFound/> }
-                    />
-                </Routes>
-            </main>
-            <footer class="flex pb-2">
-                <div class="flex-1 text-left pl-3">
-                </div>
-                <div class="flex-grow-[2] text-center">
-                    <span>"Created using: "</span>
-                    <a class="link" target="_blank" href="https://www.rust-lang.org/">
-                        <img class="w-8 inline" src="/static/rustacean-flat-gesture.svg" type="image/svg+xml" />
-                    </a>
-                    <span>" + "</span>
-                    <a class="link" target="_blank" href="https://github.com/leptos-rs/leptos">
-                        "leptos"
-                    </a>
-                    <span>" + "</span>
-                    <a class="link" target="_blank" href="https://tailwindcss.com/">
-                        "tailwindcss"
-                    </a>
-                </div>
-                <div class="flex-1 text-right pr-3">
-                    <a class="link" target="_blank" href="https://github.com/cavemanjay/cavemanjay.github.io">
-                        "Source"
-                    </a>
-                </div>
-            </footer>
-        </Router>
+        <div class="h-[97vh] overflow-auto container mx-auto">
+            <Router>
+                // <nav>
+                //     <A exact=true href=Page::Home.path()>"Home"</A>
+                //     <A exact=true href=Page::About.path()>"About"</A>
+                //     <A href=Page::Projects.path()>"Projects"</A>
+                // </nav>
+                <main class="">
+                    <Routes>
+                        <Route
+                            path=Page::Root.path()
+                            view=move |cx| view! { cx,  <Redirect path=Page::Home.path() /> }
+                        />
+                        <Route
+                            // path="/home"
+                            path=Page::Home.path()
+                            view=move |cx| view! { cx,  <Home/> }
+                        />
+                        <Route
+                            // path="/about"
+                            path=Page::About.path()
+                            view=move |cx| view! { cx,  <About/> }
+                        />
+                        <Route
+                            path=Page::BrainF___.path()
+                            view=move |cx| view! { cx,  <UnderConstruction /> }
+                        />
+                        <Route
+                            path=Page::JSF___.path()
+                            view=move |cx| view! { cx,  <JsfVisualizer /> }
+                        />
+                        <Route
+                            path=Page::TrueJson.path()
+                            view=move |cx| view! { cx,  <UnderConstruction /> }
+                        />
+                        <Route
+                            path="*"
+                            view=move |cx| view! { cx,  <NotFound/> }
+                        />
+                    </Routes>
+                </main>
+                <footer class="flex h-[3vh] bg-[var(--background)]">
+                    <div class="flex-1 text-left pl-3">
+                    </div>
+                    <div class="flex-grow-[2] text-center">
+                        <span>"Created using: "</span>
+                        <a class="link" target="_blank" href="https://www.rust-lang.org/">
+                            <img class="w-8 inline" src="/static/rustacean-flat-gesture.svg" type="image/svg+xml" />
+                        </a>
+                        <span>" + "</span>
+                        <a class="link" target="_blank" href="https://github.com/leptos-rs/leptos">
+                            "leptos"
+                        </a>
+                        <span>" + "</span>
+                        <a class="link" target="_blank" href="https://tailwindcss.com/">
+                            "tailwindcss"
+                        </a>
+                    </div>
+                    <div class="flex-1 text-right pr-3">
+                        <a class="link" target="_blank" href="https://github.com/cavemanjay/cavemanjay.github.io">
+                            "Source"
+                        </a>
+                    </div>
+                </footer>
+            </Router>
+        </div>
     }
 }
--- src/pages/projects/jsf_visualizer.rs
+++ src/pages/projects/jsf_visualizer.rs
@@ -5,8 +5,6 @@ use leptos::*;
 pub fn JsfVisualizer(cx: Scope) -> impl IntoView {
     let (src, set_src) = create_signal(cx, String::from("alert(1)"));
     let (compiled, set_compiled) = create_signal(cx, String::new());
-    // let js_state = use_state(|| AttrValue::from("alert(1)"));
-    // let output_value = use_state(String::new);
 
     let compile = move |_| {
         let src = src();
@@ -16,6 +14,7 @@ pub fn JsfVisualizer(cx: Scope) -> impl IntoView {
         let result = jsfuckrs::lbp::compile(src);
         set_compiled.set(result);
     };
+
     let execute = move |_| {
         let y = js_sys::eval(&compiled()).unwrap();
         log::info!("{:?}", y);
@@ -24,6 +23,64 @@ pub fn JsfVisualizer(cx: Scope) -> impl IntoView {
     view! {
         cx,
         <div>
+            <h1 class="text-xl">"JSF*ck"</h1>
+            <details class="mt-2" open>
+                <summary>"Project Details"</summary>
+                <p>
+                    "The website "
+                    <a class="link" href="https://esolangs.org/wiki/JSFuck" target="_blank" rel="noopener noreferrer">
+                    "esolangs.org"
+                    </a>
+                    " describes JSF*ck is an "
+                    <q>"esoteric subset of the JavaScript language that uses only six distinct characters in the source code."</q>
+                </p>
+                <p>
+                    "The characters are:"
+                </p>
+                <ol class="leading-none">
+                    <li><kbd>"+"</kbd></li>
+                    <li><kbd>"!"</kbd></li>
+                    <li><kbd>"("</kbd></li>
+                    <li><kbd>")"</kbd></li>
+                    <li><kbd>"["</kbd></li>
+                    <li><kbd>"]"</kbd></li>
+                </ol>
+                <p>
+                    "I first encountered this programming language during a CTF event
+                    and later came across this video from "
+                    <a class="link" target="_blank" href="https://www.youtube.com/@LowByteProductions">
+                        "Low Byte Productions"
+                    </a>
+                    " where he builds a compiler for this esolang in JavaScript."
+                </p>
+                <p>
+                    "I ported his compiler implementation into Rust for the heck of it
+                    with the goal of replicating his logic as close as possible."
+                </p>
+                <p>
+                    "I then remembered the site "
+                    <a class="link" href="https://jsfuck.com" target="_blank" rel="noopener noreferrer">
+                        "jsfuck.com"
+                    </a>
+                    " and compared the output of my compiler with theirs (@aemkei on GitHub) and noticed
+                    that the output of the two compilers was quite different for the same input."
+                </p>
+                <p>
+                    "For the simple input of "
+                    <strong><code class="inline-code">"alert(1)"</code></strong>
+                    " the size difference between the two outputs is in the 10s of kilobytes."
+                </p>
+                <figure class="text-center">
+                    <img src="/static/compiler_output_diff.png" alt="ls command output showing size difference between the two compilers' output" />
+                    <figcaption>"Compiler output size diff"</figcaption>
+                </figure>
+                <p>
+                    "I am now working on porting aemkei's compiler to Rust
+                    and will update this page when I am done but for now you can enter 
+                    in some JS source code and transpile it to jsf*ck using the 
+                    Low Byte Productions implementation."
+                </p>
+            </details>
             <input
                 class="m-10 [writing-mode:horizontal-tb]"
                 value=src
@@ -36,7 +93,7 @@ pub fn JsfVisualizer(cx: Scope) -> impl IntoView {
                 class="border rounded p-2"
                 on:click=compile
             >
-                {"Compile"}
+                "Compile"
             </button>
             <hr/>
             <div class="max-w-lg h-80 overflow-auto border border-[var(--secondary)] rounded m-2">
@@ -46,7 +103,7 @@ pub fn JsfVisualizer(cx: Scope) -> impl IntoView {
                 class="border mx-2 rounded p-2 border-[var(--secondary)]"
                 on:click=execute
             >
-                {"Run This"}
+                "Run This"
             </button>
         </div>
     }
